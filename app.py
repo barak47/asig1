5395b17fcb06a217f6ea0e856b8e0c6b38f4d1b0
barak47 <125534699+barak47@users.noreply.github.com>
2024-09-15 21:14:28 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
 
+# app.py
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the trained model
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get data from the POST request
+    data = request.get_json()
+
+    # Convert data into appropriate format (example: list of features)
+    features = [data['area'], data['bedrooms'], data['bathrooms']]
+
+    # Make prediction
+    prediction = model.predict([features])
+
+    # Return prediction as JSON
+    return jsonify({'prediction': prediction[0]})
+
+if __name__ == '__main__':
+    app.run(debug=True)
